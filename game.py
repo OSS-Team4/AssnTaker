4f11d508fd7e9465d7e284de12258c28a3fba35c
chalkman <16882564+chalkman@users.noreply.github.com>
2020-12-02 13:48:25 +0900

    retry

--- game.py
+++ game.py
@@ -1,13 +1,17 @@
 from bangtal import *
+import copy
 
 # Objects
+
+
 class clearScreen(Object):
     def __init__(self, scene):
         super().__init__('image/stage/clear.png')
         self.locate(scene, 355, 138)
 
+
 class actionsRemained(Object):
-    def __init__(self, scene, x, y = 120):
+    def __init__(self, scene, x, y=120):
         super().__init__('image/stage/n/x.png')
         self.locate(scene, x, y)
         self.set(0)
@@ -20,10 +24,11 @@ class actionsRemained(Object):
 
 class button(Object):
     def __init__(self, scene, name, x, y):
-        super().__init__('image/buttons/' + name +'.png')
+        super().__init__('image/buttons/' + name + '.png')
         self.locate(scene, x, y)
         self.show()
         self.scene = scene
+
 
 class button_back(button):
     def __init__(self, scene):
@@ -33,6 +38,7 @@ class button_back(button):
         if action == MouseAction.CLICK:
             main.enter()
 
+
 class button_exit(button):
     def __init__(self, scene):
         super().__init__(scene, 'exit', 867, 120)
@@ -40,6 +46,7 @@ class button_exit(button):
     def onMouseAction(self, x, y, action):
         if action == MouseAction.CLICK:
             endGame()
+
 
 class button_help(button):
     def __init__(self, scene):
@@ -49,6 +56,7 @@ class button_help(button):
         if action == MouseAction.CLICK:
             helpScene.enter()
 
+
 class button_main(button):
     def __init__(self, scene):
         super().__init__(scene, 'main', 0, 0)
@@ -57,6 +65,7 @@ class button_main(button):
         if action == MouseAction.CLICK:
             main.enter()
 
+
 class button_next(button):
     def __init__(self, scene):
         super().__init__(scene, 'next', 460, 180)
@@ -64,6 +73,7 @@ class button_next(button):
     def onMouseAction(self, x, y, action):
         if action == MouseAction.CLICK:
             stage.setStage(stage.stageDetails[0]['currentStage'] + 1)
+
 
 class button_start(button):
     def __init__(self, scene):
@@ -88,20 +98,44 @@ class gameObject(Object):
         self.position = i, j
         self.locate(self.scene, *getLocation(i, j))
 
+
 class PLAYER(gameObject):
     def __init__(self, scene, i, j):
         super().__init__('image/stage/PLAYER/Player_Idle1.png', scene, i, j, 'PLAYER')
+        self.animate()
+
+    def animate(self):
+        # animation code here
+        pass
+
+    def move(self, i, j):
+        super().move(i, j)
+        # animation code here
 
     def kick(self):
+        # animation code here
         pass
+
+    def fail(self):
+        # async
+        # animation code here
+        pass
+
 
 class PROFESSOR(gameObject):
     def __init__(self, scene, i, j):
         super().__init__('image/stage/PROFESSOR/Professor_idle1.png', scene, i, j, 'PROFESSOR')
+        self.animate()
+
+    def animate(self):
+        # animation code here
+        pass
+
 
 class KEY(gameObject):
     def __init__(self, scene, i, j):
         super().__init__('image/stage/KEY/Key1.png', scene, i, j, 'KEY')
+
 
 class DOOR(gameObject):
     def __init__(self, scene, i, j):
@@ -110,16 +144,20 @@ class DOOR(gameObject):
     def open(self):
         self.hide()
 
+
 class DESK(gameObject):
     def __init__(self, scene, i, j):
         super().__init__('image/stage/DESK/Desk.png', scene, i, j, 'DESK')
 
+
 class BOOKS(gameObject):
     def __init__(self, scene, i, j):
         super().__init__('image/stage/BOOKS/Book.png', scene, i, j, 'BOOKS')
-    
+
     def destroy(self):
+        # animation code here
         self.hide()
+
 
 class SPIKE(gameObject):
     def __init__(self, scene, i, j, active):
@@ -135,8 +173,11 @@ helpScene = Scene('help', 'image/help/background.png')
 _ = button_back(helpScene)
 
 # stage
+
+
 def getLocation(i, j):
     return 350 + 65 * j, 560 - 65 * i
+
 
 class Stage(Scene):
     # gameObjects = ['GROUND', 'WALL', 'PROFESSOR', 'KEY', 'DOOR', 'DESK', 'BOOKS', 'SPIKE_ACTIVE', 'SPIKE_INACTIVE', 'SPIKE_DESK']
@@ -145,7 +186,7 @@ class Stage(Scene):
             'currentStage': 1,
             'keysInHand': 0,
         },
-        { # 1
+        {  # 1
             'object': [
                 [1, 1, 1, 1, 1, 1, 1, 1, 1],
                 [1, 1, 1, 1, 1, 0, 0, 1, 1],
@@ -160,7 +201,7 @@ class Stage(Scene):
             'actions': 23,
             'SpikeAction': False,
         },
-        { # 2
+        {  # 2
             'object': [
                 [1, 1, 1, 1, 1, 1, 1, 1, 1],
                 [1, 1, 0, 0, 0, 0, 1, 1, 1],
@@ -175,7 +216,7 @@ class Stage(Scene):
             'actions': 24,
             'SpikeAction': False,
         },
-        { # 3
+        {  # 3
             'object': [
                 [1, 1, 1, 1, 1, 1, 1, 1, 1],
                 [1, 1, 1, 1, 1, 1, 2, 0, 1],
@@ -190,7 +231,7 @@ class Stage(Scene):
             'actions': 32,
             'SpikeAction': False,
         },
-        { # 4
+        {  # 4
             'object': [
                 [1, 1, 1, 1, 1, 1, 1, 1, 1],
                 [1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -222,15 +263,18 @@ class Stage(Scene):
             v.hide()
         if len(self.stageDetails) > n:
             self.stageDetails[0]['currentStage'] = n
-            s = self.stageDetails[self.stageDetails[0]['currentStage']]
+            self.stageMap = copy.deepcopy(
+                self.stageDetails[self.stageDetails[0]['currentStage']])
+            s = self.stageMap
             o = s['object']
             self.setImage('image/stage/background/' + str(n) + '.png')
             self.action(0)
             self.stageDetails[0]['keysInHand'] = 0
             self.objects['player'].move(*s['player'])
             self.objects['player'].show()
-            for i in range(len(self.objects['objects'])):
-                del self.objects['objects'][0]
+            for _ in range(len(self.objects['objects'])):
+                self.objects['objects'][-1].hide()
+                del self.objects['objects'][-1]
             self.objects['objects'] = []
             for i, v in enumerate(o):
                 for j, x in enumerate(v):
@@ -248,7 +292,8 @@ class Stage(Scene):
                     elif x == 7:
                         self.objects['objects'].append(SPIKE(self, i, j, True))
                     elif x == 8:
-                        self.objects['objects'].append(SPIKE(self, i, j, False))
+                        self.objects['objects'].append(
+                            SPIKE(self, i, j, False))
                     elif x == 9:
                         self.objects['objects'].append(SPIKE(self, i, j, True))
                         self.objects['objects'].append(DESK(self, i, j))
@@ -266,33 +311,34 @@ class Stage(Scene):
         self.setStage(self.stageDetails[0]['currentStage'])
 
     def getObjectIndex(self, i, j, objectType):
-        for idx , v in enumerate(self.objects['objects']):
+        for idx, v in enumerate(self.objects['objects']):
             if v.position == (i, j) and v.objectType == objectType:
                 return idx
         return -1
 
     def getObjectNumber(self, i, j):
-        o = self.stageDetails[self.stageDetails[0]['currentStage']]['object']
+        o = self.stageMap['object']
         if 0 <= i < len(o) and 0 <= j < len(o[0]):
             return o[i][j]
         else:
             return 1
 
-    def action(self, n = 1):
-        c = self.stageDetails[0]['currentStage']
-        self.stageDetails[c]['actions'] -= n
-        a = self.stageDetails[c]['actions']
+    def action(self, n=1):
+        self.stageMap['actions'] -= n
+        a = self.stageMap['actions']
         self.objects['actionsRemained'][0].set(a // 10)
         self.objects['actionsRemained'][1].set(a % 10)
 
     def move(self, di, dj):
-        c = self.stageDetails[0]['currentStage']
-        s = self.stageDetails[c]
-        p = self.objects['player'].position
-        
-        to = (p[0] + di, p[1] + dj)
-        tt = (p[0] + 2 * di, p[1] + 2 * dj)
-        
+        # c = self.stageDetails[0]['currentStage']
+        s = self.stageMap
+        m = s['object']
+        p = self.objects['player']
+        o = self.objects['objects']
+
+        to = (p.position[0] + di, p.position[1] + dj)
+        tt = (p.position[0] + 2 * di, p.position[1] + 2 * dj)
+
         if s['actions'] > 0:
             self.action()
             if s['SpikeAction']:
@@ -303,65 +349,65 @@ class Stage(Scene):
             ff = self.getObjectNumber(*tt)
 
             if f == 0 or f == 7 or f == 8:
-                self.stageDetails[c]['player'] = to
-                self.objects['player'].move(*to)
+                s['player'] = to
+                p.move(*to)
             elif f == 1:
                 pass
             elif f == 2:
                 pass
             elif f == 3:
                 k = self.getObjectIndex(*to, 'KEY')
-                self.stageDetails[c]['player'] = to
-                self.objects['player'].move(*to)
-                self.stageDetails[c]['object'][to[0]][to[1]] = 0
-                self.objects['objects'][k].pick()
+                s['player'] = to
+                p.move(*to)
+                m[to[0]][to[1]] = 0
+                o[k].pick()
                 self.stageDetails[0]['keysInHand'] += 1
             elif f == 4:
                 d = self.getObjectIndex(*to, 'DOOR')
                 if self.stageDetails[0]['keysInHand']:
                     self.stageDetails[0]['keysInHand'] -= 1
-                    self.objects['objects'][d].open()
-                    self.stageDetails[c]['object'][to[0]][to[1]] = 0
-                    self.stageDetails[c]['player'] = to
-                    self.objects['player'].move(*to)
+                    o[d].open()
+                    m[to[0]][to[1]] = 0
+                    s['player'] = to
+                    p.move(*to)
             elif f == 5:
                 d = self.getObjectIndex(*to, 'DESK')
-                self.objects['player'].kick()
+                p.kick()
                 if ff == 0:
-                    self.stageDetails[c]['object'][to[0]][to[1]] = 0
-                    self.stageDetails[c]['object'][tt[0]][tt[1]] = 5
-                    self.objects['objects'][d].move(*tt)
+                    m[to[0]][to[1]] = 0
+                    m[tt[0]][tt[1]] = 5
+                    o[d].move(*tt)
                 elif ff == 7 or ff == 8:
-                    self.stageDetails[c]['object'][to[0]][to[1]] = 0
-                    self.stageDetails[c]['object'][tt[0]][tt[1]] = 9
-                    self.objects['objects'][d].move(*tt)
+                    m[to[0]][to[1]] = 0
+                    m[tt[0]][tt[1]] = 9
+                    o[d].move(*tt)
             elif f == 6:
                 b = self.getObjectIndex(*to, 'BOOKS')
-                self.stageDetails[c]['object'][to[0]][to[1]] = 0
-                self.objects['player'].kick()
+                m[to[0]][to[1]] = 0
+                p.kick()
                 if ff == 1:
-                    self.objects['objects'][b].destroy()
+                    o[b].destroy()
                 elif ff == 7:
-                    self.stageDetails[c]['object'][tt[0]][tt[1]] = 6
-                    self.objects['objects'][b].move(*tt)
-                    self.objects['objects'][b].destroy()
+                    m[tt[0]][tt[1]] = 6
+                    o[b].move(*tt)
+                    o[b].destroy()
                 else:
-                    self.stageDetails[c]['object'][tt[0]][tt[1]] = 6
-                    self.objects['objects'][b].move(*tt)
+                    m[tt[0]][tt[1]] = 6
+                    o[b].move(*tt)
             elif f == 9:
                 d = self.getObjectIndex(*to, 'DESK')
                 k = self.getObjectIndex(*to, 'SPIKE')
-                self.objects['player'].kick()
+                p.kick()
                 if ff == 0:
-                    self.stageDetails[c]['object'][to[0]][to[1]] = 8 - self.objects['objects'][k].active
-                    self.stageDetails[c]['object'][tt[0]][tt[1]] = 5
-                    self.objects['objects'][d].move(*tt)
+                    m[to[0]][to[1]] = 8 - o[k].active
+                    m[tt[0]][tt[1]] = 5
+                    o[d].move(*tt)
                 elif ff == 7 or ff == 8:
-                    self.stageDetails[c]['object'][to[0]][to[1]] = 0
-                    self.stageDetails[c]['object'][tt[0]][tt[1]] = 9
-                    self.objects['objects'][d].move(*tt)
+                    m[to[0]][to[1]] = 0
+                    m[tt[0]][tt[1]] = 9
+                    o[d].move(*tt)
 
-            r = self.objects['player'].position
+            r = p.position
             if self.getObjectNumber(r[0] + 1, r[1]) == 2 or self.getObjectNumber(r[0] - 1, r[1]) == 2 or self.getObjectNumber(r[0], r[1] + 1) == 2 or self.getObjectNumber(r[0], r[1] - 1) == 2:
                 self.cleared()
             elif self.getObjectNumber(*r) == 7:
@@ -380,6 +426,7 @@ class Stage(Scene):
                 self.move(1, 0)
             elif key == 4 or key == 83:
                 self.move(0, 1)
+
 
 stage = Stage()
 
